0c04364013338d3334b77e6e47a27d613322c775
igorlitvintsev <litvincevi@mail.ru>
2022-10-18 18:28:55 +0300

    implement the functionality of adding a user

--- src/containers/app-layout-container.tsx
+++ src/containers/app-layout-container.tsx
@@ -1,19 +1,30 @@
-import React from 'react'
+import React from 'react';
 
-import { AppLayout, IAppAction } from '../components/app-layout'
-import { UsersTableContainer } from './users-table-container'
+import { AppLayout, IAppAction } from '../components/app-layout';
+import { UsersTableContainer } from './users-table-container';
 
-import { generate } from '../store/users/thunks'
-import { useAppDispatch } from '../store'
+import { generate, add } from '../store/users/thunks';
+import { useAppDispatch } from '../store';
 
 export const AppLayoutContainer = () => {
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
 
   const actions: IAppAction[] = [
-    { key: 'generate', title: 'Сгенерировать еще раз', action: () => dispatch(generate(20)) }
-  ]
+    {
+      key: 'generate',
+      title: 'Сгенерировать еще раз',
+      action: () => dispatch(generate(20)),
+    },
+    {
+      key: 'add',
+      title: 'Добавить пользователя',
+      action: () => dispatch(add()),
+    },
+  ];
 
-  return <AppLayout actions={actions}>
-    <UsersTableContainer />
-  </AppLayout>
-}
+  return (
+    <AppLayout actions={actions}>
+      <UsersTableContainer />
+    </AppLayout>
+  );
+};
--- src/store/users/reducer.ts
+++ src/store/users/reducer.ts
@@ -1,16 +1,35 @@
-import { createReducer } from '@reduxjs/toolkit'
+import { createReducer } from '@reduxjs/toolkit';
 
-import { IUsersState } from './types'
+import { IUsersState } from './types';
 
-import { generate } from './thunks'
+import { generate, add } from './thunks';
 
 const initialState: IUsersState = {
   fetching: false,
-  users: []
-}
+  users: [],
+};
 
-export const usersReducer = createReducer(initialState, builder =>
+export const usersReducer = createReducer(initialState, (builder) =>
   builder
     .addCase(generate.pending, (state) => ({ ...state, fetching: true }))
-    .addCase(generate.fulfilled, (state, { payload }) => ({ ...state, fetching: false, users: [ ...payload ] }))
-    .addCase(generate.rejected, (state, { payload }) => ({ ...state, fetching: false, users: [] })))
+    .addCase(generate.fulfilled, (state, { payload }) => ({
+      ...state,
+      fetching: false,
+      users: [...payload],
+    }))
+    .addCase(generate.rejected, (state) => ({
+      ...state,
+      fetching: false,
+      users: [],
+    }))
+    .addCase(add.pending, (state) => ({ ...state, fetching: true }))
+    .addCase(add.fulfilled, (state, { payload }) => ({
+      ...state,
+      fetching: false,
+      users: [...payload, ...state.users],
+    }))
+    .addCase(add.rejected, (state) => ({
+      ...state,
+      fetching: false,
+    }))
+);
